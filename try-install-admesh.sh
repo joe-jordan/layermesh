d879ce52543743e86f934c938961283575875557
Joe Jordan <joe@joe-jordan.co.uk>
2016-07-03 11:34:34 +0100

    package manager autoconfirm options for dependency installation

--- try-install-admesh.sh
+++ try-install-admesh.sh
@@ -5,10 +5,10 @@
 # `make check`.
 
 if [ -n "`which brew`" ]; then INSTALL="brew install"; fi
-if [ -n "`which apt-get`" ]; then INSTALL="sudo apt-get install"; fi
-if [ -n "`which yum`" ]; then INSTALL="sudo yum install"; fi
-if [ -n "`which dnf`" ]; then INSTALL="sudo dnf install"; fi
-if [ -n "`which zypper`" ]; then INSTALL="sudo zypper install"; fi
+if [ -n "`which apt-get`" ]; then INSTALL="sudo apt-get -y install"; fi
+if [ -n "`which yum`" ]; then INSTALL="sudo yum -y install"; fi
+if [ -n "`which dnf`" ]; then INSTALL="sudo dnf -y install"; fi
+if [ -n "`which zypper`" ]; then INSTALL="sudo zypper -y install"; fi
 
 $INSTALL admesh
 
